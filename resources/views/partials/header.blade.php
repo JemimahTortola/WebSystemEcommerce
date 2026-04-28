4c3483f1fc9611b519bef9f568c6c456527c3377
jemimah tortola <jemimahtortola27@gmail.com>
2026-04-28 21:23:31 +0800

    Update user notification bell icon to match admin
    - Changed SVG icon to match admin notification bell
    - Same bell icon as admin panel now
    - Consistent look across user and admin pages

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -24,11 +24,10 @@
             @auth
             <div class="notification-wrapper">
                 <button class="header-icon" id="notificationBtn" title="Notifications">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-                        <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0 1 18 14.158V11a6.002 6.002 0 0 0-4-5.659V5a2 2 0 1 0-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 1 1-6 0v-1m6 0H9" />
                     </svg>
-                    <span class="cart-badge" id="notifBadge" style="display: none;">0</span>
+                    <span class="notification-badge" id="notifBadge" style="display: none;">0</span>
                 </button>
                 <div class="notification-dropdown" id="notifDropdown">
                     <div class="notif-header">
